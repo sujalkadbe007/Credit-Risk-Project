6e8dce7e7ed440473ed15c358946e6c4e15d07ef
Sujal Rajesh Kadbe <sujalkadbe007@gmail.com>
2025-12-04 22:00:22 +0530

    Update Paths & Load Data.py

--- Libraries/Paths & Load Data.py
+++ Libraries/Paths & Load Data.py
@@ -1,10 +1,10 @@
-// Paths
+# Paths
 
 DATA_PATH = "Credit_risk_dataset_large.csv"
 OUTPUT_DIR = "outputs"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-// 1. Load Data
+# Load Data
 df = pd.read_csv(DATA_PATH)
 print("Dataset shape:", df.shape)
 print(df.head())
